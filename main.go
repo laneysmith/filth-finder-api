ffacd6d04df32c355947dab20c27585e90f1b332
Laney Smith <lsmith.unc@gmail.com>
2019-11-09 10:38:30 -0700

    add custom search functionality

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"go-yelp-v3/yelp"
 	"io"
 	"net/http"
 	"os"
+	"strconv"
+
+	"github.com/guregu/null"
 )
 
-func res(w http.ResponseWriter, r *http.Request) {
+func custom(w http.ResponseWriter, r *http.Request) {
 	options, err := getCredentials(w)
 	if err != nil {
 		fmt.Println(err)
@@ -21,41 +25,50 @@ func res(w http.ResponseWriter, r *http.Request) {
 	longitude := r.URL.Query().Get("longitude")
 	latitude := r.URL.Query().Get("latitude")
 
-	// TODO: add coords
+	// build SearchOptions from incoming query params
+	latitudeFloat, err := strconv.ParseFloat(latitude, 64)
+	longitudeFloat, err := strconv.ParseFloat(longitude, 64)
+	generalOptions := yelp.GeneralOptions{
+		Term: term,
+	}
 	coordinateOptions := yelp.CoordinateOptions{
-		// Latitude:  null.FloatFrom(latitude),
-		// Longitude: null.FloatFrom(longitude),
+		Latitude:  null.FloatFrom(latitudeFloat),
+		Longitude: null.FloatFrom(longitudeFloat),
 	}
 	locationOptions := yelp.LocationOptions{
 		CoordinateOptions: &coordinateOptions,
 	}
 	searchOptions := yelp.SearchOptions{
 		LocationOptions: &locationOptions,
+		GeneralOptions:  &generalOptions,
 	}
 	results, err := client.DoSearch(searchOptions)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("error:", err)
 	}
 
-	io.WriteString(w, fmt.Sprintf("Found a total of %v results for \"%v\" at longitude \"%v\" and latitude \"%v\"", results.Total, term, longitude, latitude))
-	for i := 0; i < len(results.Businesses); i++ {
-		io.WriteString(w, fmt.Sprintf("<div>%v, %v</div>", results.Businesses[i].Name, results.Businesses[i].Rating))
+	bytes, err := json.Marshal(results)
+	if err != nil {
+		fmt.Println("error:", err)
 	}
+
+	io.WriteString(w, string(bytes))
 }
 
 func main() {
-	http.HandleFunc("/", res)
+	http.HandleFunc("/custom", custom)
+	// TODO: add simple search option
+	// http.HandleFunc("/simple", simple)
 	http.ListenAndServe(":8000", nil)
 }
 
 func getCredentials(w http.ResponseWriter) (options *yelp.AuthOptions, err error) {
 	c := &yelp.AuthOptions{
-		YelpClientID: os.Getenv("YELP_CLIENT_ID"),
-		YelpAPIKey:   os.Getenv("YELP_API_KEY"),
+		YelpAPIKey: os.Getenv("YELP_API_KEY"),
 	}
 
-	if c.YelpClientID == "" || c.YelpAPIKey == "" {
-		return nil, errors.New("Missing env variables for YELP_CLIENT_ID and/or YELP_API_KEY")
+	if c.YelpAPIKey == "" {
+		return nil, errors.New("Missing env variable for YELP_API_KEY")
 	}
 
 	return c, nil
