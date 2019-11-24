61cceb5182cf9d3920b99c38a0abd9463353bcc3
Laney Smith <lsmith.unc@gmail.com>
2019-11-24 09:50:11 -0700

    update dependencies

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"go-yelp-v3/yelp"
-	"io"
 	"net/http"
 	"os"
 	"strconv"
@@ -13,8 +12,15 @@ import (
 	"github.com/guregu/null"
 )
 
+func main() {
+	port := getPort()
+
+	http.HandleFunc("/custom", custom)
+	http.ListenAndServe(":"+port, nil)
+}
+
 func custom(w http.ResponseWriter, r *http.Request) {
-	options, err := getCredentials(w)
+	options, err := getAPICredentials(w)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -42,38 +48,38 @@ func custom(w http.ResponseWriter, r *http.Request) {
 		LocationOptions: &locationOptions,
 		GeneralOptions:  &generalOptions,
 	}
+
+	// perform search
 	results, err := client.DoSearch(searchOptions)
 	if err != nil {
 		fmt.Println("error:", err)
 	}
 
-	bytes, err := json.Marshal(results)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-
-	io.WriteString(w, string(bytes))
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(results)
 }
 
-func main() {
+func getAPICredentials(w http.ResponseWriter) (options *yelp.AuthOptions, err error) {
+	o := &yelp.AuthOptions{
+		YelpAPIKey: os.Getenv("YELP_API_KEY"),
+	}
+
+	if o.YelpAPIKey == "" {
+		return nil, errors.New("Missing environment variable for YELP_API_KEY")
+	}
+
+	return o, nil
+}
+
+func getPort() string {
 	port := os.Getenv("PORT")
+
 	if port == "" {
 		port = "8080"
 		fmt.Println("No PORT environment variable detected, defaulting to " + port)
 	}
 
-	http.HandleFunc("/custom", custom)
-	http.ListenAndServe(":"+port, nil)
-}
-
-func getCredentials(w http.ResponseWriter) (options *yelp.AuthOptions, err error) {
-	c := &yelp.AuthOptions{
-		YelpAPIKey: os.Getenv("YELP_API_KEY"),
-	}
-
-	if c.YelpAPIKey == "" {
-		return nil, errors.New("Missing env variable for YELP_API_KEY")
-	}
-
-	return c, nil
+	return port
 }
